6d69340e014e4a9446c82823b9e46fa5f2338731
YohanDerenne <yohan.derenne@gmail.com>
2020-12-17 18:03:25 +0100

    Import instance

--- src/PRD/Instance.cpp
+++ src/PRD/Instance.cpp
@@ -16,9 +16,90 @@ bool Instance::Parse(string path)
 	file.open(path);
 	if (!file) {
 		throw invalid_argument("Impossible d'ouvrir le fichier en lecture");
-		return false;
 	}
+
+	// Nb cmd - Nb machines - cout vehicule - index
 	file >> n >> m >> c_V >> id;
+
+	// Coordonnées
+	vector<pair<int, int>> coord;
+
+	// Commandes
+	for (int i = 0; i < n; i++) {
+		int idCmd;
+		file >> idCmd;
+		if (idCmd != i) {
+			file.close();
+			throw exception("Format du fichier invalide");
+			return false;
+		}
+
+		// Durées
+		vector<int> t_row;
+		for (int j = 0; j < m; j++) {
+			int duree;
+			file >> duree;
+			t_row.push_back(duree);
+		}
+		t.push_back(t_row);
+
+		// Cout d'inventaire WIP
+		vector<int> h_WIP_row;
+		h_WIP_row.push_back(0);
+		for (int j = 0; j < m - 1; j++) {
+			int cout;
+			file >> cout;
+			h_WIP_row.push_back(cout);
+		}
+		h_WIP.push_back(h_WIP_row);
+
+		// Cout d'inventaire FIN
+		int cout_FIN;
+		file >> cout_FIN;
+		h_FIN.push_back(cout_FIN);
+
+		// Date livraison souhaitée
+		int date;
+		file >> date;
+		d.push_back(date);
+
+		// Penalite retard
+		int penalty;
+		file >> penalty;
+		p_M.push_back(penalty);
+
+		// Autre penalite
+		file >> penalty;
+
+		// Destination cmd
+		int x_dest;
+		int y_dest;
+		file >> x_dest >> y_dest;
+		coord.push_back({ x_dest, y_dest });
+	}
+
+	// Coord prod
+	int x_prod;
+	int y_prod;
+	file >> x_prod >> y_prod;
+
+	// Coord distrib
+	int x_distrib;
+	int y_distrib;
+	file >> x_distrib >> y_distrib;
+
+	coord.insert(coord.begin(), { x_distrib ,y_distrib });
+	coord.insert(coord.begin(), { x_prod ,y_prod });
+
+	// Calcul Distances 
+	for (int i = 0; i < coord.size(); i++) {
+		vector<int> row;
+		for (int j = 0; j < coord.size(); j++) {
+			row.push_back(sqrt(pow(coord[i].first - coord[j].first, 2) + pow(coord[i].second - coord[j].second, 2)));
+		}
+		t.push_back(row);
+	}
+
 	file.close();
 
     return true;
