a279f5fd20ee0459d5abd02395d6c4d99d9dd8f7
YohanDerenne <yohan.derenne@gmail.com>
2021-01-06 17:08:18 +0100

    Create Result Class

---
+++ src/PRD/Result.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Instance.h"
+class Result
+{
+public :
+
+	// ============================= DATA ====================================
+
+	Instance inst;
+	double cout_total;
+	double cout_WIP;
+	double cout_FIN;
+	double cout_retard;
+
+	// ============================== METHODES ===================================
+
+	Result() {};
+};
+
